702f23f5f7f7d90c8ac9fa0e0fad81b1396673dc
Ilya Kostrikov <kostrikov@Ilyas-MacBook-Air.local>
2021-12-25 15:12:40 -0800

    Replace mujoco_py with dm_control.

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ extras = {
     "accept-rom-license": ["autorom[accept-rom-license]~=0.4.2"],
     "box2d": ["box2d-py==2.3.5", "pyglet>=1.4.0"],
     "classic_control": ["pyglet>=1.4.0"],
-    "mujoco": ["mujoco_py>=1.50, <2.0"],
-    "toy_text": ["pygame==2.1.0", "scipy>=1.4.1"],
+    "mujoco": ["dm_control>=0.0.403778684"],
+    "toy_text": ["scipy>=1.4.1"],
     "other": ["lz4>=3.1.0", "opencv-python>=3.0"],
 }
 
